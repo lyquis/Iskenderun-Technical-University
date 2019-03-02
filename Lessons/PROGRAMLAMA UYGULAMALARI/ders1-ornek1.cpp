0b0190678d7cffe2a3059374e83dda0120e2610f
Bahadır A. Güder <a.bahadir.guder@gmail.com>
2019-03-02 21:20:57 +0300

    Update.

---
+++ Lessons/PROGRAMLAMA UYGULAMALARI/ders1-ornek1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+// <cstring>: String türünde verileri işleme için kullanılır.
+#include <cstring>
+
+using namespace std;
+
+int main() {
+
+    // Değişkenlerimizi tanımladık.
+    int iDiziUzunlugu = 100;
+    char cKarakterDizisi[iDiziUzunlugu];
+
+    /* Kullanıcıdan bir kelime girmesini istedik ve
+     * bu kelimeyi dizimizin içerisinde tuttuk. */
+    cout<<"Give me a word please: ";
+    cin >> cKarakterDizisi;
+
+    /* For döngüsü yardımıyla dizimizin bütün indislerini
+     * tek tek ekrana yazdırdık. Böylece bütün kelimemizi
+     * ekrana yazdırmış olduk.*/
+
+    // strlen(): Başlangıç adresini aldığı yazının uzunluğunu döndürür.
+    for (int i = strlen(cKarakterDizisi) - 1 ; i >= 0 ; i--) {
+        cout << cKarakterDizisi[i];
+    }
+
+    return 0;
+};
