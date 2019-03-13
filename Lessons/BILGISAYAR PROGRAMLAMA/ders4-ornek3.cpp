f5ddb24bf3e1b62e47207b4860c24eab9c1af456
Bahadır A. Güder <a.bahadir.guder@gmail.com>
2019-03-13 23:39:11 +0300

    Update.

--- Lessons/BILGISAYAR PROGRAMLAMA/ders4-ornek3.cpp
+++ Lessons/BILGISAYAR PROGRAMLAMA/ders4-ornek3.cpp
@@ -1,22 +1,21 @@
-// this kullanımı.fonksiyonu çağıran nesneyi gösterir  
+// This: fonksiyonu çağıran nesneyi gösterir.
 
 #include <iostream>
 using namespace std;
 
 class testSinif {
-    int x,y;   //this buradaki x ve y'ye işaret ediyor.
+    int x,y;   //This buradaki x ve y'ye işaret ediyor.
     public:
-    testSinif(int x,int y){
+    testSinif(int x,int y) {
         this ->x =x;
         this ->y =y;
     }
-    int goruntule (){
+    int goruntule () {
         return this -> x + this -> y;
     }
 };
 
-main ()
-{
+main () {
     testSinif ob (350,150);
     cout << ob.goruntule ();
     return 0;
--- Lessons/BILGISAYAR PROGRAMLAMA/ders4-ornek4.cpp
+++ Lessons/BILGISAYAR PROGRAMLAMA/ders4-ornek4.cpp
@@ -1,4 +1,4 @@
-//call by reference
+//Call by reference.
 #include <iostream>
 using namespace std;
 
@@ -6,20 +6,22 @@ class testSinif {
     private:
         int sayi;
     public:
-        testSinif (int n) {  //sayıyı n'e eşitliyor.
+        testSinif (int n) {  //Sayıyı n'e eşitliyor.
             sayi = n;
         }
-        int deger(){
+        int deger() {
             return sayi;   //n değerini döndürüyor.
         }
 };
-//kare alma işlemi
+
+//Kare alma işlemi.
 int kare (testSinif &d){   
     return d.deger () * d.deger();
 }
-int main (){
-testSinif a (9),b (11);   
-cout << kare (a) << "\n";
-cout << kare (b) << "\n";
-return 0;
+
+int main () {
+    testSinif a (9),b (11);
+    cout << kare (a) << "\n";
+    cout << kare (b) << "\n";
+    return 0;
 }
